863471872a13383da35f6479e068c8489db13ca1
Hepheir <hepheir@gmail.com>
2023-10-26 11:29:03 +0900

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Main {
+    public static void main(String[] args) {
+        throw new NotImplementedException();
+    }
+}
